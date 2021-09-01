7bfe0326f97daecca048e667e45e4b9336390ec5
Heather <h.vanderberg92@gmail.com>
2021-08-31 20:25:34 -0500

    Creation of Dictionary App

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Dictionary Application</header>
+    </div>
+  );
+}
+
+export default App;

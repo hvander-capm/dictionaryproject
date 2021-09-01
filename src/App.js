704a81f62ee732d64d8bb9ee54986ad84e3315df
Heather <h.vanderberg92@gmail.com>
2021-08-31 20:55:54 -0500

    Update JS and CSS files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Dictionary Application</header>
+      <div className="App">
+        <header className="App-header">Dictionary</header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">Coded by Heather Vanderberg</footer>
+      </div>
     </div>
   );
 }
